3caace73de3963e6b502c35351674c43666d5fa7
ozinkevych <zinkevicsasa1@gmail.com>
2024-05-17 11:56:18 +0300

    made a header and adaptive to it

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
 const burgerMenu = document.querySelector('.burger-menu');
-const mainMenu = document.querySelector('.main-menu');
+const mainMenu = document.querySelector('.main-list');
 
 burgerMenu.addEventListener('click', () => {
     mainMenu.classList.toggle('show');
