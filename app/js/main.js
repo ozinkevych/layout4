ef34c79bed2c0e30ef46c3ecd85ae4e78171524d
ozinkevych <zinkevicsasa1@gmail.com>
2024-05-20 17:51:50 +0300

    made Content left and the registration form

--- app/js/main.js
+++ app/js/main.js
@@ -4,3 +4,12 @@ const mainMenu = document.querySelector('.main-list');
 burgerMenu.addEventListener('click', () => {
     mainMenu.classList.toggle('show');
 });
+document.querySelector('.form-btn').addEventListener('click', function (event) {
+    let form = document.querySelector('.registration-form');
+
+    form.classList.add('submitted');
+
+    if (!form.checkValidity()) {
+        event.preventDefault();
+    }
+});
