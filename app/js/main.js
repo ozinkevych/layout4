df3ce8785873f5222f8dc25440d4a02e418cfe55
ozinkevych <zinkevicsasa1@gmail.com>
2024-05-24 11:42:53 +0300

    created a video block

--- app/js/main.js
+++ app/js/main.js
@@ -1,27 +1,37 @@
+$(document).ready(function() {
+    // Burger menu toggle
+    const burgerMenu = document.querySelector('.burger-menu');
+    const mainMenu = document.querySelector('.main-list');
 
-$(document).ready(function(){
+    burgerMenu.addEventListener('click', () => {
+        mainMenu.classList.toggle('show');
+    });
+
+    // Initialize the slider
     $('.slider-container-block').slick({
-        infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
-
-        responsive:[{
-            breakpoint: 1200,
-            settings:{
-                slidesToShow: 2,
-                dots: true
-            }
-        },
-        {
+        autoplay: true,
+        autoplaySpeed: 4000,
+        responsive: [
+            {
+                breakpoint: 850,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 1
+                    slidesToShow: 1,
+                    slidesToScroll: 1
                 }
             }
-        ],
+        ]
     });
-});
-$(document).ready(function() {
+
+    // Initialize the testimonials slider
     $('.testimonials-slider').slick({
         autoplay: true,
         autoplaySpeed: 4000,
@@ -34,21 +44,17 @@ $(document).ready(function() {
         cssEase: 'linear',
         draggable: false,
         swipe: false,
-        swipeToSlide: true,
+        swipeToSlide: true
     });
-});
-const burgerMenu = document.querySelector('.burger-menu');
-const mainMenu = document.querySelector('.main-list');
 
-burgerMenu.addEventListener('click', () => {
-    mainMenu.classList.toggle('show');
-});
-document.querySelector('.form-btn').addEventListener('click', function (event) {
-    let form = document.querySelector('.registration-form');
+    // Form validation
+    document.querySelector('.form-btn').addEventListener('click', function(event) {
+        let form = document.querySelector('.registration-form');
 
-    form.classList.add('submitted');
+        form.classList.add('submitted');
 
-    if (!form.checkValidity()) {
-        event.preventDefault();
-    }
+        if (!form.checkValidity()) {
+            event.preventDefault();
+        }
+    });
 });
